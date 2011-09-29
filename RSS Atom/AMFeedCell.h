c39bd22cf27352c6e012c430fe2735ca657b89e9
Shashank Patel <patel.shashank2001@gmail.com>
2011-09-29 21:49:44 +0530

    Feed list view with cell image animation implemented

---
+++ RSS Atom/AMFeedCell.h
@@ -0,0 +1,13 @@
+//
+//  AMFeedCell.h
+//  RSS Atom
+//
+//  Created by Shashank Patel on 9/29/11.
+//  Copyright 2011 Not Applicable. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMFeedCell : UITableViewCell
+
+@end
---
+++ RSS Atom/AMFeedCell.m
@@ -0,0 +1,29 @@
+//
+//  AMFeedCell.m
+//  RSS Atom
+//
+//  Created by Shashank Patel on 9/29/11.
+//  Copyright 2011 Not Applicable. All rights reserved.
+//
+
+#import "AMFeedCell.h"
+
+@implementation AMFeedCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
